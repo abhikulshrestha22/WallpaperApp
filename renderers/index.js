8eb79cf68600d6439a010c7e2422d71d88ed1949
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2017-01-29 03:02:48 +0530

    wallpaper changing from the local location of the file, next change the directory to save pic

--- renderers/index.js
+++ renderers/index.js
@@ -1,14 +1,46 @@
+var command = require('../services/command');
+var download = require('../services/download');
+
+
+
 document.getElementById('btnChangeWallpaper').addEventListener("click",function(){
-    require('../services/command').abc("node -v",(err,stdout,stderr)=>{
-        if (err) {
-    console.error(`exec error: ${err}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);});
 
 
-    require('../services/download').downloadImage("https://source.unsplash.com/random");
+// When button is clicked 
+// Download the image 
+// and change the wallpaper
+
+//callback function when download is completed
+    
+
+
+    //download image
+    download.downloadImage("https://source.unsplash.com/random",function(data){
+
+        if(data==="success"){
+
+            command.abc("gsettings set org.gnome.desktop.background picture-uri file:///home/abhishek/Desktop/Projects/wallpaperapp/file.jpg",(err,stdout,stderr)=>{
+                if (err) {
+                    console.error(`exec error: ${err}`);
+                    return;
+                    }
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);});
+
+        }
+    });
+
+    
+
+
+
+    
+
+
+    
+
+    
+
 
 }
 );
--- services/download.js
+++ services/download.js
@@ -2,11 +2,31 @@ var https = require('../node_modules/follow-redirects').https;
 var fs = require('fs');
 
 
-module.exports.downloadImage = (url)=>{
-var file = fs.createWriteStream("file.jpg");
+module.exports.downloadImage = (url,cb)=>{
+
 
 var request = https.get(url,function(res){
-    res.pipe(file);
+    if(res.statusCode ===200){
+        var file = fs.createWriteStream("file.jpg");
+        res.pipe(file);
+
+        file.on('finish',function(){
+            file.close();
+            //console.log("file is complete & closed now");
+            cb("success");
+        })
+    }
+    else{
+        //console.log("Cannot fetch the image.");
+        cb("error");
+    }
+
+    //add timeout to the request 
+
+    request.setTimeout(12000,function(){
+        request.abort();
+    })
+    
 });
 
 }
